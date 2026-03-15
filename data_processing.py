ab2b50bb80e6ce4ef6016abb6b7ca31e538f0f24
Rovince Gangwar <rovince2803@gmail.com>
2026-03-15 23:12:14 +0530

    started pdf text extraction using PyMuPDF

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import sqlite3
 
+import fitz
+
+
+# CSV DATA
+
 # data pre-processing
 dataframe = pd.read_csv("./data/ecommerce_data.csv")
 dataframe["InvoiceDate"] = pd.to_datetime(dataframe["InvoiceDate"])
@@ -10,3 +15,13 @@ connection = sqlite3.connect("database.db")
 dataframe.to_sql("ecommerce_table", connection, if_exists="replace")
 
 connection.close()
+
+
+# PDF DATA
+
+policy_file_path = "./data/ecommerce_policies.pdf"
+policy_doc = fitz.open(policy_file_path)
+
+for i, page in enumerate(policy_doc):
+    text = page.get_text()
+    # print(f"Page {i}:\n\n{text}\n\n")
