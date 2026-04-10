d3d4dec748f5f48bfc5966ac628d649270db5f77
Rovince Gangwar <rovince2803@gmail.com>
2026-04-10 12:19:48 +0530

    ecom agent workflow working from direct execution

--- agents/stt_agent/agent.py
+++ agents/stt_agent/agent.py
@@ -1,4 +1,6 @@
 import logging
+import asyncio
+
 import numpy as np
 
 from agents.stt_agent.utils.record import record_with_vad
@@ -8,8 +10,10 @@ logger = logging.getLogger(__name__)
 
 
 async def stt_agent():
-    logger.debug("STT agent...")
-    recorded_frames = record_with_vad()
-    converted_text = speech_to_text(np.concatenate(recorded_frames, axis=0).squeeze())
+    logger.info("STT agent...")
+    recorded_frames = await asyncio.to_thread(record_with_vad)
+    if not recorded_frames:
+        return None
+    converted_text = await asyncio.to_thread(speech_to_text, np.concatenate(recorded_frames, axis=0).squeeze())
 
     return converted_text
--- workflows/ecom_agentic_workflow.py
+++ workflows/ecom_agentic_workflow.py
@@ -1,3 +1,5 @@
+from utils.logger_config import setup_logging
+setup_logging()
 import logging
 
 from agents.rag_agent.agent import ollama_rag_agent
@@ -6,10 +8,20 @@ from agents.stt_agent.agent import stt_agent
 logger = logging.getLogger(__name__)
 
 
-while(True):
-    recorded_text = await stt_agent()
-    agent_response = await ollama_rag_agent(
-        user_query=recorded_text
-        application_context=application_context
-    )
-    logger.info(f"Response: {agent_response}")
+from uuid import uuid4
+application_context = {
+    "session_id": uuid4()
+}
+
+async def execute_workflow():
+    while(True):
+        logger.info("Executing agentic workflow...")
+        recorded_text = await stt_agent()
+        agent_response = await ollama_rag_agent(
+            user_query=recorded_text,
+            application_context=application_context
+        )
+        logger.info(f"Response: {agent_response}")
+
+import asyncio
+asyncio.run(execute_workflow())
