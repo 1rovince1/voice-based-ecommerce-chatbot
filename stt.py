11d93e6ff658de94ce693365de673b8f7f43c5cb
Rovince Gangwar <rovince2803@gmail.com>
2026-04-11 23:44:27 +0530

    updated requirements, deleted some files

--- stt.py
+++
@@ -1,9 +0,0 @@
-# import whisper
-
-# stt_model = whisper.load_model("base")
-
-# import sounddevice as sd
-
-import requests
-result = requests.get("https://www.google.com/search?q=google+messages")
-print(result.content)
